5f0d1c0ee7a7a86b52a0abc75856944efca91ddb
datsuns <the.skeleton7@gmail.com>
2024-03-12 22:40:41 +0900

    replace to files that template generated

---
+++ src/client/java/me/datsuns/simplecoordinate/SimpleCoordinatesClient.java
@@ -0,0 +1,10 @@
+package me.datsuns.simplecoordinate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleCoordinatesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
