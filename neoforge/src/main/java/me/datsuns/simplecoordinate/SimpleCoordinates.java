695e64a67997c763dcc644345eb3193241544c21
datsuns <the.skeleton7@gmail.com>
2025-12-16 21:46:33 +0900

    fix event registration for NeoForge
    TODO:
      other events may improve registration

--- neoforge/src/main/java/me/datsuns/simplecoordinate/SimpleCoordinates.java
+++ neoforge/src/main/java/me/datsuns/simplecoordinate/SimpleCoordinates.java
@@ -1,12 +1,12 @@
 package me.datsuns.simplecoordinate;
 
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.client.event.RenderGuiEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,12 +17,11 @@ import me.datsuns.simplecoordinate.config.ModKeyBinding;
 public class SimpleCoordinates {
     public static final String MOD_ID = "simplecoordinates";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    static final ResourceLocation HUD_LAYER = ResourceLocation.fromNamespaceAndPath(MOD_ID, "coordinates");
 
     public SimpleCoordinates(IEventBus modEventBus) {
         modEventBus.addListener(ModClientEvents::onClientSetup);
         modEventBus.addListener(ModClientEvents::onRegisterKeyMappings);
-        modEventBus.addListener(ModClientEvents::onRegisterLayers);
+        NeoForge.EVENT_BUS.addListener(ModClientEvents::onRenderGui);
         NeoForge.EVENT_BUS.addListener(ClientEvents::onClientTick);
     }
 
@@ -35,8 +34,8 @@ public class SimpleCoordinates {
             ModKeyBinding.register(event);
         }
 
-        public static void onRegisterLayers(RegisterGuiLayersEvent event) {
-            event.registerAboveAll(HUD_LAYER, CoordinateRenderer::render);
+        public static void onRenderGui(RenderGuiEvent.Post event) {
+            CoordinateRenderer.render(event.getGuiGraphics(), event.getPartialTick());
         }
     }
 
